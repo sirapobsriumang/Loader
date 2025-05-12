d9eab17d1f27fb33aa7c2fc6f5ab33ece993db3d
Neverwinlol <beekidnapped@gmail.com>
2025-05-12 09:53:40 +0000

    Test Loader

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,17 @@
--- Wait i will do it later
+local gameId = game.GameId
+
+if gameId == 210851291 then -- Build a Boat
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sirapobsriumang/Build-a-boat-for-Treasure/refs/heads/main/Farm.lua"))()
+
+elseif gameId == 4777817887 then -- Blade Ball
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sirapobsriumang/Blade-ball/main/Blade-ball-free.lua"))()
+    
+    elseif gameId == 2404080894 then -- Funky Friday
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sirapobsriumang/Funky-Friday/refs/heads/main/Auto.lua"))()
+
+else
+    loadstring(game:GetHttp("https://raw.githubusercontent.com/sirapobsriumang/Universal/refs/heads/main/Universal.lua"))()
+end
